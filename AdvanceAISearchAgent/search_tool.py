18b7bdc100a4e8c1c7a30225ce50169cc3728d95
mdsaibhossain <saibhossain5@gmail.com>
2025-08-25 02:22:50 +0600

    search

---
+++ AdvanceAISearchAgent/search_tool.py
@@ -0,0 +1,10 @@
+# from duckduckgo_search import DDGS
+from ddgs import DDGS
+
+def search_duckduckgo(queary:str,num_reslts: int =5):
+    results=[]
+    with DDGS() as ddgs:
+        for r in ddgs.text(queary,max_results=num_reslts):
+            results.append(f"{r['title']} - {r['href']}\n{r['body']}")
+
+    return "\n\n".join(results)
