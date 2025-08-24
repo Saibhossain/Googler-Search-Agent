3f694ab149e213f1c6451a61e40b755bd9cba794
mdsaibhossain <saibhossain5@gmail.com>
2025-08-25 02:49:10 +0600

    search and summary  agent

---
+++ AdvanceAISearchAgent/agent_flow.py
@@ -0,0 +1,41 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+from LLM import ollama_generate
+from search_tool import search_duckduckgo
+
+
+
+class AgentState(TypedDict, total=False):
+    query: str
+    search_results: str
+    final_answer: str
+
+def search_node(state: AgentState):
+    query = state["query"]
+    search_results = search_duckduckgo(query)
+    state["search_results"] = search_results
+    return state
+
+def summarize_node(state: AgentState):
+    query = state["query"]
+    results = state["search_results"]
+
+    prompt = f"""You are a helpful assistant.
+                Search Query: {query}
+                Results: {results} Summarize the findings clearly:"""
+    summary = ollama_generate(prompt)
+    state["final_answer"] = summary
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("Search",search_node)
+graph.add_node("Summarize",summarize_node)
+
+graph.set_entry_point("Search")
+graph.add_edge("Search","Summarize")
+graph.add_edge("Summarize",END)
+agent = graph.compile()
+
+query = "Latest research on Bangla LLMs"
+result = agent.invoke({"query": query})
+print(result["final_answer"])
